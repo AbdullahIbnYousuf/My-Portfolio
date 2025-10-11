7a15af70e7cff10e6d1fab630c823837e39e14db
AbdullahIbnYousuf <abuabdullahibnyousuf@gmail.com>
2025-10-11 18:54:03 +0600

    Uploading to github

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  return (
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}></motion.div>
+  );
+};
+
+export default Hero;
